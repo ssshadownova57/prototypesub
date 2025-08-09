bd90f9f9c66a13b6b58f307f2be9eae49aacbcb4
Indigocoder1 <caleb@liston.us>
2025-08-08 18:25:04 -0700

    Swap out Alembic goo for blend shape goo

---
+++ PrototypeSubMod/MiscMonobehaviors/BlendShapePlayer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace PrototypeSubMod.MiscMonobehaviors;
+
+public class BlendShapePlayer : MonoBehaviour
+{
+    [SerializeField] private SkinnedMeshRenderer renderer;
+    [SerializeField] private int shapeIndex;
+    [SerializeField] private float initialValue;
+    [SerializeField] private float speed;
+
+    private float timeVal;
+
+    private void Start()
+    {
+        timeVal = initialValue;
+    }
+
+    private void Update()
+    {
+        timeVal += Time.deltaTime * speed;
+        var weight = Mathf.PingPong(timeVal, 100);
+
+        renderer.SetBlendShapeWeight(shapeIndex, weight);
+    }
+}
---
+++ PrototypeSubMod/MiscMonobehaviors/RotateOverTime.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace PrototypeSubMod.MiscMonobehaviors;
+
+public class RotateOverTime : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private Vector3 initialVector;
+    [SerializeField] private float timeBetweenVectorChanges;
+    [SerializeField] private float rotationSpeed;
+    [SerializeField] private float axisAlignmentSpeed;
+
+    private Vector3 currentAxis;
+    private Vector3 startAxis;
+    private Vector3 targetAxis;
+    private Vector3 poleAxis;
+    private float axisChangeCountdown;
+    private float currentRotationTime;
+    private float currentAngle;
+    
+    private void Start()
+    {
+        targetAxis = initialVector.normalized;
+        startAxis = target.eulerAngles;
+        poleAxis = (targetAxis + startAxis) * 0.5f;
+    }
+
+    private void Update()
+    {
+        if (axisChangeCountdown < timeBetweenVectorChanges)
+        {
+            axisChangeCountdown += Time.deltaTime;
+        }
+        else
+        {
+            axisChangeCountdown = 0;
+            startAxis = currentAxis.normalized;
+            poleAxis = targetAxis.normalized;
+            targetAxis = Random.onUnitSphere.normalized;
+        }
+
+        if (currentRotationTime < rotationSpeed)
+        {
+            currentRotationTime += Time.deltaTime;
+        }
+        else
+        {
+            currentRotationTime = 0;
+        }
+
+        currentAngle += Time.deltaTime * rotationSpeed;
+        target.rotation = Quaternion.AngleAxis(currentAngle, currentAxis.normalized);
+        float normalizedProgress = currentRotationTime / rotationSpeed;
+        var seg1Axis = Vector3.Lerp(startAxis, poleAxis, normalizedProgress).normalized;
+        var seg2Axis = Vector3.Lerp(poleAxis, targetAxis, normalizedProgress).normalized;
+        var finalAxis = Vector3.Lerp(seg1Axis, seg2Axis, normalizedProgress).normalized;
+        currentAxis = Vector3.Lerp(currentAxis, finalAxis, axisAlignmentSpeed * Time.deltaTime);
+    }
+}
