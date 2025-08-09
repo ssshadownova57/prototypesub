dd679e1a6c1e0f6f09a2f04d74b404b502a07a54
Indigocoder1 <caleb@liston.us>
2025-08-08 17:57:53 -0700

    Remove power fallback thing

--- PrototypeSubMod/PowerSystem/PrototypePowerSystem.cs
+++ PrototypeSubMod/PowerSystem/PrototypePowerSystem.cs
@@ -145,8 +145,6 @@ public class PrototypePowerSystem : MonoBehaviour, ISaveDataListener, IProtoTree
         }
 
         UpdateRelayStatus();
-        
-        HandleFallbackPowerIssues();
     }
 
     public static void AddPowerSource(TechType techType, PowerConfigData configData)
